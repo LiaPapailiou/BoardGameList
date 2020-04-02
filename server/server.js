53767df98050262ee92cf4d7a22706c4437ab9d7
LiaPapailiou <LiaPapailiou@users.noreply.github.com>
2020-04-03 00:01:11 +0200

    made two more routes

--- server/server.js
+++ server/server.js
@@ -20,4 +20,24 @@ app.get('/api/boardgames/description/:gameId',  (req,res) => {
     .then((data) => res.send(data));
 });
 
+// get ID from second API
+app.get('/api/boardgames/:name', (req, res) => {
+  fetch(`https://www.boardgameatlas.com/api/search?name=${req.params.name}&client_id=GRYCkBFss8`)
+    .then((res) => res.json())
+    .then((data) => {
+    const id = JSON.stringify(data.games[0].id);
+    res.send(id);
+  });
+});
+
+// get video with the previous ID
+app.get('/api/boardgames/video/:gameId', (req, res) => {
+  fetch(`https://www.boardgameatlas.com/api/game/videos?limit=20&client_id=GRYCkBFss8&game_id=${req.params.gameId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const video = JSON.stringify(data.videos[0].url);
+      res.send(video);
+    });
+
+});
 app.listen(9000);
