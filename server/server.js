8d04dee1dc2b8cfbfdfdf8e1cf6cfc41c77e2ccb
LiaPapailiou <LiaPapailiou@users.noreply.github.com>
2020-04-03 06:59:43 +0200

    handling errors in the backend

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const fetch = require('node-fetch');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const port = 9000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,13 +12,25 @@ app.use(bodyParser.json());
 app.get('/api/boardgames', (req, res) =>{
   fetch(`https://bgg-json.azurewebsites.net/hot`)
     .then((res) => res.json())
-    .then((data) => res.send(data));
+    .then((data) => {
+      if(!data) {
+        res.status(404).send('Resource Not Found');
+        return;
+      }
+      res.send(data);
+    });
 });
 
 app.get('/api/boardgames/description/:gameId',  (req,res) => {
    fetch(`https://bgg-json.azurewebsites.net/thing/${req.params.gameId}`)
     .then((res) => res.json())
-    .then((data) => res.send(data));
+    .then((data) =>  {
+       if(!data) {
+      res.status(404).send('Resource Not Found');
+      return;
+    }
+    res.send(data);
+  });
 });
 
 // get ID from second API
@@ -25,6 +38,10 @@ app.get('/api/boardgames/:name', (req, res) => {
   fetch(`https://www.boardgameatlas.com/api/search?name=${req.params.name}&client_id=GRYCkBFss8`)
     .then((res) => res.json())
     .then((data) => {
+      if(!data) {
+        res.status(404).send('Resource Not Found');
+        return;
+      }
     const id = JSON.stringify(data.games[0].id);
     res.send(id);
   });
@@ -35,9 +52,28 @@ app.get('/api/boardgames/video/:gameId', (req, res) => {
   fetch(`https://www.boardgameatlas.com/api/game/videos?limit=20&client_id=GRYCkBFss8&game_id=${req.params.gameId}`)
     .then((res) => res.json())
     .then((data) => {
-      const video = JSON.stringify(data.videos[0].url);
-      res.send(video);
+      if(!data) {
+        res.status(404).send('Resource Not Found');
+        return;
+      }
+      const videoLink = JSON.stringify(data.videos[0].url);
+      res.send(videoLink);
     });
-
 });
-app.listen(9000);
+
+// GLOBAL ERROR HANDLER
+app.use((req, res, next) => {
+  const error = new Error('Internal Server Error');
+  error.status = 500;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status);
+  res.send(err.message);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`CORS-enabled web server listening on port ${port}`);
+});
